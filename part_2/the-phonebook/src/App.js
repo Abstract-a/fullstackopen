0a2f9b60096449e8260f5fc475dfe21386708b7a
Med Km <m.kamari100@gmail.com>
2023-07-11 23:32:18 +0100

    finished the-phonebook exercises

--- part_2/the-phonebook/src/App.js
+++ part_2/the-phonebook/src/App.js
@@ -1,11 +1,22 @@
 import { useState } from 'react';
+import Filter from './components/Filter';
+import PersonForm from './components/PersonForm';
+import Persons from './components/Persons';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '2124444' },
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
   ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
+
+  const filteredPersons = persons.filter((item) => {
+    return item.name.toLowerCase().includes(filter.toLowerCase());
+  });
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -23,6 +34,10 @@ const App = () => {
     setNewNumber('');
   }
 
+  function handleFilter(e) {
+    setFilter(e.target.value);
+  }
+
   function handleNameChange(e) {
     setNewName(e.target.value);
   }
@@ -33,25 +48,19 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-          <div>
-            number:
-            <input value={newNumber} onChange={handleNumberChange} />
-          </div>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <div></div>
-      <h2>Numbers</h2>
-      {persons.map((person) => (
-        <p key={person.name}>
-          {person.name} : {person.number}
-        </p>
-      ))}
+
+      <Filter filter={filter} handleChange={handleFilter} />
+      <h1>Add a new</h1>
+
+      <PersonForm
+        submit={handleSubmit}
+        nameValue={newName}
+        nameChange={handleNameChange}
+        numberValue={newNumber}
+        numberChange={handleNumberChange}
+      />
+      <h3>Numbers</h3>
+      <Persons filtered={filteredPersons} />
     </div>
   );
 };
